a4a308a352336be29f944c59199a2688f7cc6c94
Vishvang <vishvangp.8@gmail.com>
2019-04-14 18:21:55 -0400

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index(string id)
+        //{
+        //    //return View();
+        //    //return "Hello MVC";
+        //    string queryString = Request.QueryString.Get("name");
+        //    return "ID = "+id + " Query String = "+ queryString;
+        //}
+
+
+        public string Index(string id,string name)
+        {
+            return "ID = " + id + " Name = " + name;
+        }
+
+        public string GetResult() {
+            return "My String from Get Result";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
